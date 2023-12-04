18e6e6557cd5169c4fed0c3cde71bd8dd202bf6c
md-jim <151822477+md-jim@users.noreply.github.com>
2023-12-04 20:02:49 +0600

    Add files via upload

---
+++ 4. middle num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter two int number:");
+scanf("\n %d \n %d",&a,&b);
+
+if(a>b)
+
+printf("%d is maximum number.",a);
+
+else {if(b>a)
+printf("%d is maximum number.",b);}
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+}
